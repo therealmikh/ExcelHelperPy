7b13833bba7596715e8617ee3f118dd9a01892ae
Michael Clifford <92742268+therealmikh@users.noreply.github.com>
2021-11-07 14:05:04 +0300

    Upload main.py
    Главный файл

---
+++ main.py
@@ -0,0 +1,20 @@
+# import module
+import pandas as pd
+
+######### [ НАСТРОЙКИ ] #########
+FILE_NAME = "test2.xlsx"        # ИМЯ ФАЙЛА, КОТОРЫЙ НУЖНО ОТКРЫТЬ (ЗАКИНУТЬ В ПАПКУ С ПРОГРАММОЙ)
+FILE_OUTPUT = "output.xlsx"     # ИМЯ ФАЙЛА ДЛЯ СОХРАНЕНИЯ (СОХРАНИТСЯ В ПАПКЕ С ПРОГРАММОЙ)
+#################################
+
+# Excel Helper Engine
+
+try:
+    df = pd.read_excel(str(FILE_NAME))          # FILE NAME
+    res = df.sort_values('Время договора').groupby('Наименование инструмента').tail(1)  # ФИЛЬТРАЦИЯ PANDAS
+    res.to_excel(FILE_OUTPUT, index=False)    # FILE OUTPUT                                       
+    print('Complete!')
+except NameError:
+    print("Error")
+
+
+
